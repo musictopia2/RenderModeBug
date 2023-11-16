87fafe62f455c9ab91066c0d08abc54df8f6a75e
Andy Walter <musictopia2@hotmail.com>
2023-11-16 08:40:23 -0700

    Add project files.

---
+++ BlazorNative/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorNative;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ BlazorNative/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,8 @@
+﻿using Foundation;
+
+namespace BlazorNative;
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
